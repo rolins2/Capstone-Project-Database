9e09c5ed2f04ac3b0f482fa011d6c242203b9658
rolins2 <127750238+rolins2@users.noreply.github.com>
2024-02-16 01:28:53 +0530

    render

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const app = express();
 
 const port = 3000;
 
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+})
+
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`);
 })
