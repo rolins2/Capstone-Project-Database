2bfb417c760e1057b23cce2be33fd19643a01687
rolins2 <127750238+rolins2@users.noreply.github.com>
2024-02-18 23:23:05 +0530

    es

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import pg from 'pg';
 import  bodyParser from 'body-parser';
+import axios from 'axios';
 
 const app = express();
 
 const port = 3000;
 
+const url = "https://covers.openlibrary.org/b/isbn/0385472579-S.jpg";
 app.get("/",(req,res)=>{
     res.render("index.ejs");
 })
@@ -13,5 +15,6 @@ app.get("/",(req,res)=>{
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`);
 })
+
 
 //can use isbn code to get the image
